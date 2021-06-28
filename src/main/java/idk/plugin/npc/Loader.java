3616e61924f9cabaea89153b4b4a7900af9e3af4
ChrixWX92 <chris@wards-net.co.uk>
2021-06-28 23:30:47 +0100

    Needs default Steve/Alex texture picker tweaking (probably).

--- src/main/java/idk/plugin/npc/Loader.java
+++ src/main/java/idk/plugin/npc/Loader.java
@@ -3,18 +3,42 @@ package idk.plugin.npc;
 import cn.nukkit.Server;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.plugin.PluginManager;
+import cn.nukkit.utils.TextFormat;
 import idk.plugin.npc.commands.NpcCommand;
 import idk.plugin.npc.listeners.entity.*;
 import idk.plugin.npc.listeners.entity.player.*;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class Loader extends PluginBase {
 
+    public static Loader plugin;
+
+    public String getPath() {
+        String PATH = "\\skins";
+        Path currentRelativePath = Paths.get("");
+        String imgPath = currentRelativePath.toAbsolutePath().toString();
+        return imgPath.concat(PATH);
+    }
+
+    public File directory = new File(getPath());
+
     @Override
     public void onEnable() {
+        plugin = this;
         NPC.registerEntity();
         PluginManager pluginManager = Server.getInstance().getPluginManager();
+        getServer().getConsoleSender().sendMessage(TextFormat.BLUE+"NPC plugin enabled");
+
+        if (! directory.exists()){
+            directory.mkdir();
+            getServer().getConsoleSender().sendMessage(TextFormat.GOLD + getPath() + " created for custom NPC skins.");
+        }
+        else {getServer().getConsoleSender().sendMessage(TextFormat.GREEN + "Skins folder found.");}
+
         if(pluginManager.getPlugin("FormAPI") == null) {
             getLogger().alert("§cRequired component not found (FormAPI Plugin)");
             pluginManager.disablePlugin(this);
@@ -38,3 +62,11 @@ public class Loader extends PluginBase {
         Server.getInstance().getCommandMap().register("", new NpcCommand());
     }
 }
+
+
+
+
+
+
+
+
--- src/main/java/idk/plugin/npc/commands/NpcCommand.java
+++ src/main/java/idk/plugin/npc/commands/NpcCommand.java
@@ -7,11 +7,20 @@ import cn.nukkit.command.data.CommandParamType;
 import cn.nukkit.command.data.CommandParameter;
 import cn.nukkit.entity.Entity;
 import cn.nukkit.entity.EntityHuman;
+import cn.nukkit.entity.data.Skin;
 import cn.nukkit.inventory.PlayerInventory;
 import cn.nukkit.nbt.tag.CompoundTag;
+import cn.nukkit.utils.TextFormat;
+import idk.plugin.npc.Loader;
+import io.netty.util.internal.ThreadLocalRandom;
 import ru.nukkitx.forms.elements.CustomForm;
 
-import java.util.UUID;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
 
 import static idk.plugin.npc.NPC.*;
 
@@ -25,7 +34,44 @@ public class NpcCommand extends Command {
                 });
         this.setPermission("npc.use");
     }
+/*
+    public static void sendSetNPCSkinPacket(Entity npc, Player player, String username) { // The username is the name for the player that has the skin.
+        removeNPCPacket(npc, player);
 
+        GameProfile profile = new GameProfile(uuid, null);
+
+        try {
+            HttpsURLConnection connection = (HttpsURLConnection) new URL(String.format("https://api.ashcon.app/mojang/v2/user/%s", username)).openConnection();
+            if (connection.getResponseCode() == HttpsURLConnection.HTTP_OK) {
+                ArrayList<String> lines = new ArrayList<>();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                reader.lines().forEach(lines::add);
+
+                String reply = String.join(" ",lines);
+                int indexOfValue = reply.indexOf("\"value\": \"");
+                int indexOfSignature = reply.indexOf("\"signature\": \"");
+                String skin = reply.substring(indexOfValue + 10, reply.indexOf("\"", indexOfValue + 10));
+                String signature = reply.substring(indexOfSignature + 14, reply.indexOf("\"", indexOfSignature + 14));
+
+                profile.getProperties().put("textures", new Property("textures", skin, signature));
+            }
+
+            else {
+                player.getServer().broadcastMessage("Connection could not be opened when fetching player skin (Response code " + connection.getResponseCode() + ", " + connection.getResponseMessage() + ")");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // The client settings.
+        DataWatcher watcher = npc.getDataWatcher();
+        watcher.set(new DataWatcherObject<>(15, DataWatcherRegistry.a), (byte)127);
+        PacketPlayOutEntityMetadata packet = new PacketPlayOutEntityMetadata(npc.getId(), watcher, true);
+        ((CraftPlayer)player).getHandle().playerConnection.sendPacket(packet);
+
+        addNPCPacket(npc, player);
+    }
+*/
     @Override
     public boolean execute(CommandSender sender, String s, String[] args) {
         if (!this.testPermission(sender)) {
@@ -59,6 +105,8 @@ public class NpcCommand extends Command {
                 CustomForm customForm = new CustomForm("§l§8Create NPC")
                         .addDropDown("§l§7Entity Type", entityList, 16)
                         .addInput("§l§7Entity Name")
+                        .addInput("§l§7Custom Skin")
+                        .addSlider("Size",1,100,1,10)
                         .addToggle("§l§fRotаtion", true)
                         .addToggle("§l§fNametag visibilitу", true)
                         .addInput("§l§7Commands (Across ,)", "cmd1, cmd2, cmd3")
@@ -71,19 +119,74 @@ public class NpcCommand extends Command {
 
                     String entityType = (String) data.get(0);
                     String entityName = (String) data.get(1);
-                    boolean isRotation = (Boolean) data.get(2);
-                    boolean visibleTag = (Boolean) data.get(3);
-                    String[] commands = ((String) data.get(4)).split(", ");
-                    boolean isPlayer = (Boolean) data.get(5);
-                    boolean hasUseItem = entityType.equals("Human") ? (Boolean) data.get(7) : false;
+                    String customSkin = (String) data.get(2);
+                    Float scale = (Float) data.get(3);
+                    boolean isRotation = (Boolean) data.get(4);
+                    boolean visibleTag = (Boolean) data.get(5);
+                    String[] commands = ((String) data.get(6)).split(", ");
+                    boolean isPlayer = (Boolean) data.get(7);
+                    boolean hasUseItem = entityType.equals("Human") ? (Boolean) data.get(9) : false;
+
+                    Skin nsStatic = new Skin();
+                    BufferedImage skinFile = null;
+                    String filePath;
+                    Skin oldSkin = new Skin ();
+
+                    if (customSkin.length() < 1) {
+                        int prots = ThreadLocalRandom.current().nextInt(0, 2); //TODO: Working?
+                        if (prots == 1) {
+                            InputStream skinSteve = getClass().getResourceAsStream("/steve.png");
+                            try {
+                                skinFile = ImageIO.read(skinSteve);
+                            } catch (IOException ioException) {
+                                ioException.printStackTrace();
+                            }
+                        }
+                        else{
+                            InputStream skinAlex = getClass().getResourceAsStream("/alex.png");
+                            try {skinFile = ImageIO.read(skinAlex);}
+                            catch (IOException ioException) {ioException.printStackTrace();}
+
+
+                            oldSkin.setSkinData(skinFile);
+                            player.setSkin(oldSkin);
+                            player.getServer().updatePlayerListData(playerUniqueId, player.getId(), player.getName(), oldSkin);
+                            nsStatic = oldSkin;
+                        }
+                    }
+                    else {
+                        String nameError = new StringBuilder().append(TextFormat.RED + "No " + args[0] + "file found in " + Loader.plugin.directory.toString()).append(". Be sure to include the file extension when specifying a skin (e.g. 'Herobrine.png', rather than 'Herobrine''.)").toString();
+                        try {
+                            filePath = Loader.plugin.directory.toString().concat("\\").concat(customSkin);
+                            File skinPath = (new File(filePath));
+                            if (skinPath.exists()) {skinFile = ImageIO.read(skinPath);}
+                            else{sender.sendMessage(nameError);}
+                        } catch (IOException ioException) {
+                            sender.sendMessage(nameError);
+                        }
+
+                        Skin newSkin = new Skin();
+
+                        newSkin.setSkinData(skinFile);
+
+                        player.setSkin(newSkin);
+                        player.getServer().updatePlayerListData(playerUniqueId, player.getId(), player.getName(), newSkin);
+
+                        nsStatic = newSkin;
+                    }
+
+
                     CompoundTag compoundTag = nbt(player, entityType, commands, isPlayer, isRotation);
 
                     Entity entity = Entity.createEntity(entityType + "NPC", player.chunk, compoundTag);
                     if (!entityName.replace(" ", "").equals("")) {
                         entity.setNameTag(entityName);
                     }
+
                     entity.setNameTagVisible(visibleTag);
                     entity.setNameTagAlwaysVisible(visibleTag);
+
+                    entity.setScale((scale / 10));
 
                     if (entityType.equals("Human")) {
                         EntityHuman human = (EntityHuman) entity;
@@ -96,9 +199,19 @@ public class NpcCommand extends Command {
                         }
                     }
 
-                    entity.spawnToAll();
                     sender.sendMessage("§fNPC §aspawned§f with ID §e" + entity.getId() + " §fand the name §b\"§f" + entity.getName() + "§b\"");
+
+                    player.getServer().updatePlayerListData(playerUniqueId, player.getId(), player.getName(), nsStatic);
+
+                    player.hidePlayer(player);
+                    player.showPlayer(player);
+
+                    entity.spawnToAll();
+
+                    entity.recalculateBoundingBox();
+
                 });
+
                 break;
 
             case "getid":
